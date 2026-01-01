11282b73828a125d4a8ebc3b4dbed216b5dc8991
Steven Clontz <steven.clontz@gmail.com>
2026-01-01 23:04:02 +0000

    update default new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,83 +18,34 @@ class ProjectsController < ApplicationController
     @project = Project.new user: @current_user, title: "New Project"
     @project.content = <<-eos
 <?xml version="1.0" encoding="UTF-8"?>
+
 <section>
-<title>My Section</title>
-<p>
-  This is a paragraph.  It can contain <term>terms</term> and <em>emphasis</em>.
-</p>
-<fact>
-<p>
-This is a fact.
-</p>
-</fact>
-<corollary>
-<title>My Corollary</title>
-<p>
-  This is a paragraph in a section.  It can contain <term>terms</term> and <em>emphasis</em>.
-</p>
-</corollary>
-<theorem>
-<title>My Theorem</title>
-<p>
-  This is a paragraph in a section.  It can contain <term>terms</term> and <em>emphasis</em>.
-</p>
-</theorem>
-<p>Lemma text here</p>
-<p>Another paragraph</p>
-<p>
-  Welcome to this very basic demo of how tiptap can be used to edit PreTeXt.  First, a definition.
-</p>
+  <title> Welcome to PreTeXt.Plus! </title>
 
-<conjecture>
-<title>Title of Conjecture</title>
+  <p>
+    This is a sample project to get you started. You can edit this content using the PreTeXt markup language.
+    <me>
+      \\left|\\sum_{i=0}^n a_i\\right|\\leq\\sum_{i=0}^n|a_i|
+    </me>
+  </p>
 
-<p>
-A <term>conjecture</term> is somethign you hope is true.
-</p>
-<p> Another paragraph </p>
+  <fact>
+    <statement>
+      <p>
+        For more information on how to use PreTeXt, please visit <c>https://pretextbook.org/doc/guide/html/</c>.
+      </p>
+    </statement>
+  </fact>
 
-</conjecture>
+  <note>
+    <p>
+      Note: currently, PreTeXt.Plus only supports a subset of PreTeXt features, and only allows authoring the content of an <c>article</c>. We look forward to expanding this in the future!
+    </p>
+  </note>
 
-<definition>
-<title>Title of Definition</title>
-
-<p>
-A <term>definition block</term> is a section of text that contains a definition.
-</p>
-<p> Another paragraph </p>
-
-</definition>
-
-<assumption>
-<title>Title of Assumption</title>
-
-<p>
-An <term>assumption</term> is something you assume.
-</p>
-
-</assumption>
-
-<ul>
-  <li>
-    That's a bullet list with one …
-  </li>
-  <li>
-    … or two list items.
-  </li>
-</ul>
-
-<p>
-  Pretty neat, huh?  Oh yeah, and it can do some math: <m>\\int_1^2 x^2 dx = \\frac{7}{3}</m>.  I don't know if you can do display math though.  Perhaps <md>\\int_1^2 x^2 dx = \\frac{7}{3}</md> will work?
-</p>
-<theorem>
-  <title>My Theorem</title>
-
-  <p>This is a theorem</p>
-  <p>Another paragraph</p>
-</theorem>
-
-<p> And that's the end of the demo.  Thanks for coming!</p>
+  <p>
+    Feel free to delete this sample content and start creating your own project. Happy writing!
+  </p>
 </section>
     eos
   end
