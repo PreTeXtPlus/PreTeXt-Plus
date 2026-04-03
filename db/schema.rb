340447acbc3339702bc2fd22d73507563a7dd8ef
Oscar Levin <oscar.levin@gmail.com>
2026-04-03 22:30:06 +0000

    improve project page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_29_220404) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_03_130000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -28,11 +28,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_29_220404) do
   end
 
   create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.text "content"
     t.datetime "created_at", null: false
     t.integer "document_type"
     t.text "html_source"
-    t.text "pretext_content"
+    t.text "pretext_source"
+    t.text "source"
     t.integer "source_format", default: 0, null: false
     t.string "title"
     t.datetime "updated_at", null: false
