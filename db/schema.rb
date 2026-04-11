230b20f50378c5d7d8c79c5616edf4a16a1d536d
Steven Clontz <steven.clontz@gmail.com>
2026-04-11 19:54:15 +0000

    migrate projects to have document_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_06_203735) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_11_195312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -30,7 +30,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_06_203735) do
   create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "docinfo"
-    t.integer "document_type"
+    t.integer "document_type", default: 0, null: false
     t.text "html_source"
     t.text "pretext_source"
     t.text "source"
