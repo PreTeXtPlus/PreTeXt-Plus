77bfed2a8d32b5af43e953123959c7f4cc64f753
Steven Clontz <steven.clontz@gmail.com>
2026-04-15 14:37:38 +0000

    use stripe info

--- app/models/subscription_type.rb
+++ app/models/subscription_type.rb
@@ -9,6 +9,24 @@ class SubscriptionType < ApplicationRecord
     stripe_price_id.present?
   end
 
+  def stripe_price
+    Stripe::Price.retrieve(stripe_price_id)
+  end
+
+  def price
+    return "Free!" if stripe_price_id.blank?
+    ActiveSupport::NumberHelper.number_to_currency(stripe_price.unit_amount / 100.0, unit: "$", precision: 0)
+  end
+
+  def recurrence
+    return nil if stripe_price_id.blank?
+    stripe_price.recurring.interval
+  end
+
+  def self.of_subscription(subscription)
+    SubscriptionType.find_by stripe_price_id: subscription.processor_plan
+  end
+
   private
     def normalize_orders
       if order.present?
