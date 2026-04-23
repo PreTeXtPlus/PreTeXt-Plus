30d9b023b7b4fb1739197660a5eacc26c230fbc6
Oscar Levin <oscar.levin@gmail.com>
2026-04-23 18:33:22 +0000

    fix missing comma

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -13,7 +13,7 @@ class FeedbackMailer < ApplicationMailer
     mail(
       to: "feedback@pretext.plus",
       subject: "PreTeXt.Plus Feedback: #{@context}",
-      from: "feedbackform@mailer.pretext.plus"
+      from: "feedbackform@mailer.pretext.plus",
       reply_to: @email
     )
   end
