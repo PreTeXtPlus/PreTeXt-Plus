4850f97183bba9bcd90aef43a312a97f8b0fddde
Steven Clontz <steven.clontz@gmail.com>
2026-06-11 15:27:27 +0000

    add back comment on modern browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
   before_action :authenticate_user!
+
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   def self.allow_unauthenticated_access(**options)
