c7f7e93a7302199e0df842ac4c3c79cb1baa2bcc
Steven Clontz <steven.clontz@gmail.com>
2026-06-16 16:08:07 +0000

    set ref regex constant

--- app/models/division.rb
+++ app/models/division.rb
@@ -3,11 +3,14 @@ class Division < ApplicationRecord
   enum :source_format, { pretext: 0, latex: 1, markdown: 2 }, default: :pretext, suffix: true, validate: true
 
   validates :is_root, uniqueness: { scope: :project_id, message: "root division already exists for this project" }, if: :is_root?
+
+  validates :ref, format: REF_REGEX, presence: true, uniqueness: { scope: :project }
+
+  before_create :set_default_source
+
   DEFAULT_PRETEXT_SOURCE = File.read Rails.root.join("app", "default_docs", "pretext.xml")
   DEFAULT_LATEX_SOURCE = File.read Rails.root.join("app", "default_docs", "latex.tex")
   DEFAULT_MARKDOWN_SOURCE = File.read Rails.root.join("app", "default_docs", "markdown.md")
-
-  before_create :set_default_source
 
   def set_default_source
     unless source.present?
--- app/models/project_asset.rb
+++ app/models/project_asset.rb
@@ -3,7 +3,7 @@ class ProjectAsset < ApplicationRecord
   accepts_nested_attributes_for :library_asset
   belongs_to :project
   validates_uniqueness_of :library_asset, scope: :project
-  validates :ref, format: /\A[a-zA-z\_][a-zA-Z0-9\-\_]*\z/, presence: true, uniqueness: { scope: :project }
+  validates :ref, format: REF_REGEX, presence: true, uniqueness: { scope: :project }
 
   def url
     library_asset.url
