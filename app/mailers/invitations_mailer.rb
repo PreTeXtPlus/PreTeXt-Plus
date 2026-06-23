f9487d21059c090c656a9c2b455af211b99d3f40
Steven Clontz <steven.clontz@gmail.com>
2026-06-23 17:54:57 +0000

    don't require invites

--- app/mailers/invitations_mailer.rb
+++
@@ -1,8 +0,0 @@
-class InvitationsMailer < ApplicationMailer
-  def invite(email, inviter, invitee_user = nil)
-    @invitee_user = invitee_user
-    @email = email
-    @inviter = inviter
-    mail subject: "You've been invited to PreTeXt.Plus!", to: email
-  end
-end
--- app/models/request.rb
+++
@@ -1,5 +0,0 @@
-class Request < ApplicationRecord
-  belongs_to :user
-
-  validates_uniqueness_of :user_id
-end
