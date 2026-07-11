16634877dd695b61dd83f7a5503deb50ae536ebb
Steven Clontz <steven.clontz@gmail.com>
2026-07-11 18:06:44 +0000

    sort builds by created_at by default

--- app/models/build.rb
+++ app/models/build.rb
@@ -4,4 +4,6 @@ class Build < ApplicationRecord
   has_many :build_files, dependent: :destroy
 
   enum :status, { pending: 0, in_progress: 1, success: 2, failed: 3, sent_to_server: 4, received_from_server: 5 }, default: :pending, validate: true
+
+  default_scope { order(created_at: :desc) }
 end
