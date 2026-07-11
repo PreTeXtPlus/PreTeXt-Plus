ea4fae1b200a5a41e6a59ea67c3145412ebdbac4
Steven Clontz <steven.clontz@gmail.com>
2026-07-11 18:44:26 +0000

    display times in local timezone with UTC titletext

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,22 @@ module ApplicationHelper
     html = Commonmarker.to_html(markdown_text)
     html.html_safe
   end
+
+  # Renders a <time> tag whose visible text is upgraded to the browser's
+  # local timezone via the "local-time" Stimulus controller. The initial
+  # (pre-JS) text and the tooltip are both rendered in UTC server-side.
+  def local_time_tag(datetime, date_only: false)
+    utc = datetime.utc
+    format = date_only ? "%B %-d, %Y" : "%B %-d, %Y at %-I:%M %p"
+
+    tag.time(
+      utc.strftime(format),
+      datetime: utc.iso8601,
+      title: "#{utc.strftime(format)} UTC",
+      data: {
+        controller: "local-time",
+        local_time_date_only_value: date_only
+      }
+    )
+  end
 end
