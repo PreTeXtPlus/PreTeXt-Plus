54a59ee0c0e123144711eb6df9a892c69205f913
Steven Clontz <steven.clontz@gmail.com>
2026-08-01 13:30:03 -0500

    Change CSS theme from 'greeley' to 'default-modern'

--- packages/web-editor/src/components/wasmPreview.ts
+++ packages/web-editor/src/components/wasmPreview.ts
@@ -105,7 +105,7 @@ export async function renderPreviewHtml(
 ): Promise<PreviewRender> {
   const { renderHtml } = await loadRenderer();
   const { html, sourceMap } = await renderHtml({
-    cssTheme: "greeley",
+    cssTheme: "default-modern",
     sourcePath: PREVIEW_SOURCE_PATH,
     projectDir: "/source",
     sourceContent: source,
