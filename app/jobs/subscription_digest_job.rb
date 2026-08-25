6c57edb2e37ed1798fca346b1c1f51ba84ffd90f
Steven Clontz <steven.clontz@gmail.com>
2026-08-25 23:14:44 +0000

    don't email subscription notice when no subscription made

--- app/jobs/subscription_digest_job.rb
+++ app/jobs/subscription_digest_job.rb
@@ -3,6 +3,8 @@ class SubscriptionDigestJob < ApplicationJob
 
   def perform
     since = 1.day.ago
+    return if Pay::Subscription.where("created_at >= ?", since).none?
+
     User.where(admin: true).each do |admin|
       AdminDigestMailer.subscription_digest(admin.email, since).deliver_later
     end
