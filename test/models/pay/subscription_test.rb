c6fa074137b1b2b7bff095e9c6af25822d4084eb
Steven Clontz <steven.clontz@gmail.com>
2026-08-27 22:13:36 +0000

    don't give subscriber benefits for unpaid invoices

--- test/models/pay/subscription_test.rb
+++ test/models/pay/subscription_test.rb
@@ -10,4 +10,22 @@ class Pay::SubscriptionTest < ActiveSupport::TestCase
     user = users(:subscribed)
     assert user.subscribed?
   end
+
+  test "invoiced subscription with unpaid invoice does not grant privileges" do
+    subscription = pay_subscriptions(:one)
+    subscription.object = { "collection_method" => "send_invoice", "latest_invoice" => { "status" => "open" } }
+    assert_not subscription.grants_privileges?
+  end
+
+  test "invoiced subscription with paid invoice grants privileges" do
+    subscription = pay_subscriptions(:one)
+    subscription.object = { "collection_method" => "send_invoice", "latest_invoice" => { "status" => "paid" } }
+    assert subscription.grants_privileges?
+  end
+
+  test "non-invoiced active subscription grants privileges regardless of latest_invoice" do
+    subscription = pay_subscriptions(:one)
+    subscription.object = { "collection_method" => "charge_automatically", "latest_invoice" => { "status" => "open" } }
+    assert subscription.grants_privileges?
+  end
 end
