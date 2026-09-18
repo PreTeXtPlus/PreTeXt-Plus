9b93e322520bd3191ab6d897955933c8d39c642c
Steven Clontz <steven.clontz@gmail.com>
2026-09-18 18:15:29 +0000

    default 0 trial days

--- db/migrate/20260918120000_replace_trial_date_with_trial_days.rb
+++ db/migrate/20260918120000_replace_trial_date_with_trial_days.rb
@@ -1,10 +1,6 @@
 class ReplaceTrialDateWithTrialDays < ActiveRecord::Migration[8.1]
   def up
     add_column :subscription_types, :trial_days, :integer, default: 0, null: false
-    execute <<~SQL.squish
-      UPDATE subscription_types SET trial_days = 7
-      WHERE stripe_price_id IS NOT NULL AND stripe_price_id <> ''
-    SQL
     remove_column :subscription_types, :trial_date
   end
 
